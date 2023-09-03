cd3d07fdf76a8d50c677a20476c27567bf7fecf7
Alexandr Kuchin <kuchin.aleksandr@gmail.com>
2023-09-04 01:53:52 +0400

    added DiffUtil for RecyclerView

--- app/src/main/java/com/example/cryptoapp/presentation/adapters/CoinInfoViewHolder.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/adapters/CoinInfoViewHolder.kt
@@ -1,4 +1,4 @@
-package com.example.cryptoapp.presentation
+package com.example.cryptoapp.presentation.adapters
 
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cryptoapp.databinding.ItemCoinInfoBinding
